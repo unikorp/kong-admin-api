808576c278226944abe701f03455d74921363534
VEBER Arnaud <arnaud@veber.pw>
2017-08-10 10:55:26 +0200

    feat(api): update api node

--- tests/Document/ApiTest.php
+++ tests/Document/ApiTest.php
@@ -87,143 +87,108 @@ class ApiTest extends TestCase
     }
 
     /**
-     * test set request host
+     * test set hosts
      *
      * @return void
      *
-     * @covers \Unikorp\KongAdminApi\Document\Api::setRequestHost
+     * @covers \Unikorp\KongAdminApi\Document\Api::setHosts
      */
-    public function testSetRequestHost()
+    public function testSetHosts()
     {
         // asserts
-        $this->document->setRequestHost('test');
-        $this->assertSame('test', $this->readAttribute($this->document, 'requestHost'));
+        $this->document->setHosts('test');
+        $this->assertSame('test', $this->readAttribute($this->document, 'hosts'));
     }
 
     /**
-     * test get request host
+     * test get hosts
      *
      * @return void
      *
-     * @covers \Unikorp\KongAdminApi\Document\Api::getRequestHost
+     * @covers \Unikorp\KongAdminApi\Document\Api::getHosts
      */
-    public function testGetRequestHost()
+    public function testGetHosts()
     {
         // reflect `document`
         $reflectionClass = new \ReflectionClass($this->document);
 
-        // set `request host` property from `document` accessible
-        $reflectionProperty = $reflectionClass->getProperty('requestHost');
+        // set `hosts` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('hosts');
         $reflectionProperty->setAccessible(true);
 
         // assert
         $reflectionProperty->setValue($this->document, 'test');
-        $this->assertSame('test', $this->document->getRequestHost());
+        $this->assertSame('test', $this->document->getHosts());
     }
 
     /**
-     * test set request path
+     * test set uris
      *
      * @return void
      *
-     * @covers \Unikorp\KongAdminApi\Document\Api::setRequestPath
+     * @covers \Unikorp\KongAdminApi\Document\Api::setUris
      */
-    public function testSetRequestPath()
+    public function testSetUris()
     {
         // asserts
-        $this->document->setRequestPath('test');
-        $this->assertSame('test', $this->readAttribute($this->document, 'requestPath'));
+        $this->document->setUris('test');
+        $this->assertSame('test', $this->readAttribute($this->document, 'uris'));
     }
 
     /**
-     * test get request path
+     * test get uris
      *
      * @return void
      *
-     * @covers \Unikorp\KongAdminApi\Document\Api::getRequestPath
+     * @covers \Unikorp\KongAdminApi\Document\Api::getUris
      */
-    public function testGetRequestPath()
+    public function testGetUris()
     {
         // reflect `document`
         $reflectionClass = new \ReflectionClass($this->document);
 
-        // set `request path` property from `document` accessible
-        $reflectionProperty = $reflectionClass->getProperty('requestPath');
+        // set `uris` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('uris');
         $reflectionProperty->setAccessible(true);
 
         // assert
         $reflectionProperty->setValue($this->document, 'test');
-        $this->assertSame('test', $this->document->getRequestPath());
+        $this->assertSame('test', $this->document->getUris());
     }
 
     /**
-     * test set strip request path
+     * test set methods
      *
      * @return void
      *
-     * @covers \Unikorp\KongAdminApi\Document\Api::setStripRequestPath
+     * @covers \Unikorp\KongAdminApi\Document\Api::setMethods
      */
-    public function testSetStripRequestPath()
+    public function testSetMethods()
     {
         // asserts
-        $this->document->setStripRequestPath(true);
-        $this->assertSame(true, $this->readAttribute($this->document, 'stripRequestPath'));
+        $this->document->setMethods('test');
+        $this->assertSame('test', $this->readAttribute($this->document, 'methods'));
     }
 
     /**
-     * test get strip request path
+     * test get methods
      *
      * @return void
      *
-     * @covers \Unikorp\KongAdminApi\Document\Api::getStripRequestPath
+     * @covers \Unikorp\KongAdminApi\Document\Api::getMethods
      */
-    public function testGetStripRequestPath()
+    public function testGetMethods()
     {
         // reflect `document`
         $reflectionClass = new \ReflectionClass($this->document);
 
-        // set `strip request path` property from `document` accessible
-        $reflectionProperty = $reflectionClass->getProperty('stripRequestPath');
+        // set `methods` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('methods');
         $reflectionProperty->setAccessible(true);
 
         // assert
-        $reflectionProperty->setValue($this->document, true);
-        $this->assertSame(true, $this->document->getStripRequestPath());
-    }
-
-    /**
-     * test set preserve host
-     *
-     * @return void
-     *
-     * @covers \Unikorp\KongAdminApi\Document\Api::setPreserveHost
-     */
-    public function testSetPreserveHost()
-    {
-        // asserts
-        $this->document->setPreserveHost(true);
-        $this->assertSame(true, $this->readAttribute($this->document, 'preserveHost'));
-    }
-
-    /**
-     * test get preserve host
-     *
-     * @return void
-     *
-     * @covers \Unikorp\KongAdminApi\Document\Api::getPreserveHost
-     */
-    public function testGetPreserveHost()
-    {
-        // reflect `document`
-        $reflectionClass = new \ReflectionClass($this->document);
-
-        // set `preserve host` property from `document` accessible
-        $reflectionProperty = $reflectionClass->getProperty('preserveHost');
-        $reflectionProperty->setAccessible(true);
-
-        // assert
-        $reflectionProperty->setValue($this->document, true);
-        $this->assertSame(true, $this->document->getPreserveHost());
+        $reflectionProperty->setValue($this->document, 'test');
+        $this->assertSame('test', $this->document->getMethods());
     }
 
     /**
@@ -262,6 +227,286 @@ class ApiTest extends TestCase
     }
 
     /**
+     * test set strip uri
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::setStripUri
+     */
+    public function testSetStripUri()
+    {
+        // asserts
+        $this->document->setStripUri(false);
+        $this->assertSame(false, $this->readAttribute($this->document, 'stripUri'));
+    }
+
+    /**
+     * test get strip uri
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::getStripUri
+     */
+    public function testGetStripUri()
+    {
+        // reflect `document`
+        $reflectionClass = new \ReflectionClass($this->document);
+
+        // set `strip uri` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('stripUri');
+        $reflectionProperty->setAccessible(true);
+
+        // assert
+        $reflectionProperty->setValue($this->document, false);
+        $this->assertSame(false, $this->document->getStripUri());
+    }
+
+    /**
+     * test set preserve host
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::setPreserveHost
+     */
+    public function testSetPreserveHost()
+    {
+        // asserts
+        $this->document->setPreserveHost(true);
+        $this->assertSame(true, $this->readAttribute($this->document, 'preserveHost'));
+    }
+
+    /**
+     * test get preserve host
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::getPreserveHost
+     */
+    public function testGetPreserveHost()
+    {
+        // reflect `document`
+        $reflectionClass = new \ReflectionClass($this->document);
+
+        // set `preserve host` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('preserveHost');
+        $reflectionProperty->setAccessible(true);
+
+        // assert
+        $reflectionProperty->setValue($this->document, true);
+        $this->assertSame(true, $this->document->getPreserveHost());
+    }
+
+    /**
+     * test set retries
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::setRetries
+     */
+    public function testSetRetries()
+    {
+        // asserts
+        $this->document->setRetries(10);
+        $this->assertSame(10, $this->readAttribute($this->document, 'retries'));
+    }
+
+    /**
+     * test get retries
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::getRetries
+     */
+    public function testGetRetries()
+    {
+        // reflect `document`
+        $reflectionClass = new \ReflectionClass($this->document);
+
+        // set `retries` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('retries');
+        $reflectionProperty->setAccessible(true);
+
+        // assert
+        $reflectionProperty->setValue($this->document, 10);
+        $this->assertSame(10, $this->document->getRetries());
+    }
+
+    /**
+     * test set upstream connect timeout
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::setUpstreamConnectTimeout
+     */
+    public function testSetUpstreamConnectTimeout()
+    {
+        // asserts
+        $this->document->setUpstreamConnectTimeout(10000);
+        $this->assertSame(10000, $this->readAttribute($this->document, 'upstreamConnectTimeout'));
+    }
+
+    /**
+     * test get upstream connect timeout
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::getUpstreamConnectTimeout
+     */
+    public function testGetUpstreamConnectTimeout()
+    {
+        // reflect `document`
+        $reflectionClass = new \ReflectionClass($this->document);
+
+        // set `upstream connect timeout` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('upstreamConnectTimeout');
+        $reflectionProperty->setAccessible(true);
+
+        // assert
+        $reflectionProperty->setValue($this->document, 10000);
+        $this->assertSame(10000, $this->document->getUpstreamConnectTimeout());
+    }
+
+    /**
+     * test set upstream send timeout
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::setUpstreamSendTimeout
+     */
+    public function testSetUpstreamSendTimeout()
+    {
+        // asserts
+        $this->document->setUpstreamSendTimeout(10000);
+        $this->assertSame(10000, $this->readAttribute($this->document, 'upstreamSendTimeout'));
+    }
+
+    /**
+     * test get upstream send timeout
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::getUpstreamSendTimeout
+     */
+    public function testGetUpstreamSendTimeout()
+    {
+        // reflect `document`
+        $reflectionClass = new \ReflectionClass($this->document);
+
+        // set `upstream send timeout` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('upstreamSendTimeout');
+        $reflectionProperty->setAccessible(true);
+
+        // assert
+        $reflectionProperty->setValue($this->document, 10000);
+        $this->assertSame(10000, $this->document->getUpstreamSendTimeout());
+    }
+
+    /**
+     * test set upstream read timeout
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::setUpstreamReadTimeout
+     */
+    public function testSetUpstreamReadTimeout()
+    {
+        // asserts
+        $this->document->setUpstreamReadTimeout(10000);
+        $this->assertSame(10000, $this->readAttribute($this->document, 'upstreamReadTimeout'));
+    }
+
+    /**
+     * test get upstream read timeout
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::getUpstreamReadTimeout
+     */
+    public function testGetUpstreamReadTimeout()
+    {
+        // reflect `document`
+        $reflectionClass = new \ReflectionClass($this->document);
+
+        // set `upstream send timeout` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('upstreamReadTimeout');
+        $reflectionProperty->setAccessible(true);
+
+        // assert
+        $reflectionProperty->setValue($this->document, 10000);
+        $this->assertSame(10000, $this->document->getUpstreamReadTimeout());
+    }
+
+    /**
+     * test set https only
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::setHttpsOnly
+     */
+    public function testSetHttpsOnly()
+    {
+        // asserts
+        $this->document->setHttpsOnly(true);
+        $this->assertSame(true, $this->readAttribute($this->document, 'httpsOnly'));
+    }
+
+    /**
+     * test get https only
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::getHttpsOnly
+     */
+    public function testGetHttpsOnly()
+    {
+        // reflect `document`
+        $reflectionClass = new \ReflectionClass($this->document);
+
+        // set `https only` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('httpsOnly');
+        $reflectionProperty->setAccessible(true);
+
+        // assert
+        $reflectionProperty->setValue($this->document, true);
+        $this->assertSame(true, $this->document->getHttpsOnly());
+    }
+
+    /**
+     * test set http if terminated
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::setHttpIfTerminated
+     */
+    public function testSetHttpIfTerminated()
+    {
+        // asserts
+        $this->document->setHttpIfTerminated(false);
+        $this->assertSame(false, $this->readAttribute($this->document, 'httpIfTerminated'));
+    }
+
+    /**
+     * test get http if terminated
+     *
+     * @return void
+     *
+     * @covers \Unikorp\KongAdminApi\Document\Api::getHttpIfTerminated
+     */
+    public function testGetHttpIfTerminated()
+    {
+        // reflect `document`
+        $reflectionClass = new \ReflectionClass($this->document);
+
+        // set `http if terminated` property from `document` accessible
+        $reflectionProperty = $reflectionClass->getProperty('httpIfTerminated');
+        $reflectionProperty->setAccessible(true);
+
+        // assert
+        $reflectionProperty->setValue($this->document, false);
+        $this->assertSame(false, $this->document->getHttpIfTerminated());
+    }
+
+    /**
      * test to json
      *
      * @return void
@@ -274,13 +519,21 @@ class ApiTest extends TestCase
     {
         $this->document
             ->setName('name')
-            ->setRequestHost('requestHost')
-            ->setStripRequestPath(true)
+            ->setHosts('hosts')
+            ->setUris('uris')
+            ->setMethods('methods')
+            ->setUpstreamUrl('upstreamUrl')
+            ->setStripUri(false)
             ->setPreserveHost(true)
-            ->setUpstreamUrl('upstreamUrl');
+            ->setRetries(10)
+            ->setUpstreamConnectTimeout(10000)
+            ->setUpstreamSendTimeout(10000)
+            ->setUpstreamReadTimeout(10000)
+            ->setHttpsOnly(true)
+            ->setHttpIfTerminated(false);
 
         $this->assertSame(
-            '{"name":"name","request_host":"requestHost","strip_request_path":true,"preserve_host":true,"upstream_url":"upstreamUrl"}',
+            '{"name":"name","hosts":"hosts","uris":"uris","methods":"methods","upstream_url":"upstreamUrl","strip_uri":false,"preserve_host":true,"retries":10,"upstream_connect_timeout":10000,"upstream_send_timeout":10000,"upstream_read_timeout":10000,"https_only":true,"http_if_terminated":false}',
             $this->document->toJson()
         );
     }
